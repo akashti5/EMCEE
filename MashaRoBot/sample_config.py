04378dcc7acb77542a9f004fe7974bf33c2916d4
akashti5 <78689979+akashti5@users.noreply.github.com>
2021-05-23 00:07:45 +0530

    Update sample_config.py

--- MashaRoBot/sample_config.py
+++ MashaRoBot/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1731770725  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "fakefriends01"
+    SUPPORT_CHAT = "Singing_cartoons_007"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -538978927
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -538978927
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
